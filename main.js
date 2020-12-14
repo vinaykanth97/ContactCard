c8ef3411bfde06bf5c5b035233b8097a3127e88c
vinaypump <vinaykennedy@gmail.com>
2020-12-14 22:52:33 +0530

    Added new features

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let contactList = document.querySelector(".contact-list");
 let contactCard = document.querySelector(".contact-card");
-
+let addContact = document.querySelector(".add-contact");
 async function contactInfo() {
   try {
     const response = await fetch("https://gorest.co.in/public-api/users/");
@@ -15,6 +15,7 @@ contactInfo().then((users) => {
     let contactName = user.name;
     let lists = document.createElement("p");
     lists.classList.add("users");
+    lists.setAttribute("data-id", `${user.id}`);
     lists.innerText = contactName;
     contactList.appendChild(lists);
   });
@@ -27,18 +28,34 @@ function showContactCard(e) {
     });
     e.target.classList.add("active");
     contactInfo().then((users) => {
-      users.data.forEach((userData) => {
-        if (e.target.innerText === userData.name) {
+      let userFilter = users.data.filter((userData) => {
+        let getId = e.target.getAttribute("data-id");
+        let getIdNumber = parseInt(getId);
+        if (getIdNumber === userData.id) {
           let eachUser = `<p>${userData.id}</p>
-                            <p>${userData.name}</p>
-                            <p>${userData.email}</p>
-                            <p>${userData.gender}</p>
-                            <p>${userData.status}</p>
-          `;
+                                  <p>${userData.name}</p>
+                                  <p>${userData.email}</p>
+                                  <p>${userData.gender}</p>
+                                  <p>${userData.status}</p>
+                `;
           contactCard.innerHTML = eachUser;
         }
       });
     });
   }
 }
+function addNewUser(e) {
+  e.preventDefault();
+  let name = document.querySelector(".name").value;
+  let age = document.querySelector(".age").value;
+  let email = document.querySelector(".email").value;
+  //   let gender = document.querySelector(".gender option").value;
+  //   let activeStatus = document.querySelector(".active-state").value;
+  let lists = document.createElement("p");
+  lists.classList.add("users");
+  //   lists.setAttribute("data-id", `${user.id}`);
+  lists.innerText = name;
+  contactList.appendChild(lists);
+}
 contactList.addEventListener("click", showContactCard);
+// addContact.addEventListener("submit", addNewUser);
